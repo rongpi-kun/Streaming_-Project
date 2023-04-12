ea7764bf674d55e6c60bb16dee4baa35caf274ee
rongpi-kun <rongpi.kun14@gmail.com>
2023-04-12 11:03:59 +0530

    third commit

--- api/video.py
+++ api/video.py
@@ -37,26 +37,27 @@ async def insert(request: schemas.VideoData, db = Depends(get_db)):
     db.refresh(video)
     return video
 
-@router.get("/{id}")
-def stream_video(id: int, db = Depends(get_db)):
-    video = db.query(models.VideoModel).filter(models.VideoModel.id == id).first()
-    video_path = video.file_path
-    # Replace with your video file path
-    # video_file_path = "path/to/video.mp4"
+# @router.get("/{id}")
+# def stream_video(id: int, db = Depends(get_db)):
+#     video = db.query(models.VideoModel).filter(models.VideoModel.id == id).first()
+#     video_path = video.file_path
+#     # Replace with your video file path
+#     # video_file_path = "path/to/video.mp4"
 
-    def generate():
-        with open(video_path, "rb") as video_file:
-            while True:
-                chunk = video_file.read(1024*1024)
-                if not chunk:
-                    break
-                yield chunk
+#     def generate():
+#         with open(video_path, "rb") as video_file:
+#             while True:
+#                 chunk = video_file.read(1024*1024)
+#                 if not chunk:
+#                     break
+#                 yield chunk
 
-    headers = {
-            "Content-Type": "video/mp4"
-        }
+#     headers = {
+#             "Content-Type": "video/mp4",
+#             "Accept-Ranges": "bytes",
+#         }
 
-    return StreamingResponse(generate(), media_type="video/mp4", headers=headers)
+#     return StreamingResponse(generate(), media_type="video/mp4", headers=headers)
 
 # async def stream_video(id: int, db = Depends(get_db)):
 #     # replace with your own logic for getting the video file
@@ -74,22 +75,22 @@ def stream_video(id: int, db = Depends(get_db)):
 
 #     return response
 
-# @router.get("/{id}")
-# async def video_endpoint(id: int, range: str = Header(None), db = Depends(get_db)):
-#     video = db.query(models.VideoModel).filter(models.VideoModel.id == id).first()
-#     video_path = video.file_path
-#     print('current: ', video_path)
-#     print('range: ', range)
-#     start, end = range.replace("bytes=", "").split("-")
-#     start = int(start)
-#     end = int(end) if end else start + CHUNK_SIZE
-#     with open(video_path, "rb") as video:
-#         video.seek(start)
-#         data = video.read(end - start)
-#         # filesize = str(video_path.stat().st_size)
-#         filesize = os.stat(video_path).st_size
-#         headers = {
-#             'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
-#             'Accept-Ranges': 'bytes'
-#         }
-#         return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+@router.get("/{id}")
+async def video_endpoint(id: int, range: str = Header(None), db = Depends(get_db)):
+    video = db.query(models.VideoModel).filter(models.VideoModel.id == id).first()
+    video_path = video.file_path
+    print('current: ', video_path)
+    print('range: ', range)
+    start, end = range.replace("bytes=", "").split("-")
+    start = int(start)
+    end = int(end) if end else start + CHUNK_SIZE
+    with open(video_path, "rb") as video:
+        video.seek(start)
+        data = video.read(end - start)
+        # filesize = str(video_path.stat().st_size)
+        filesize = os.stat(video_path).st_size
+        headers = {
+            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+            'Accept-Ranges': 'bytes'
+        }
+        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
